09ec5c41e5a894f12aa5ea4db82f863c0e8947a2
hongqian luo <hongqianl@hotmail.com>
2023-03-04 16:22:24 -0600

    test change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello World!  I am so excited to see my first react app deployed in AWS!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
